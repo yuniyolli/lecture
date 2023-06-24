db72bc6673ca094ac686eeee7325bdbcba41cfd5
yuniyolli <yuniyolli@gmail.com>
2023-06-24 20:36:21 +0900

    add: 0706 demo, component

---
+++ 0706demo/src/main/java/com/example/demo/yes/foo/bar/baz/FooBarComponent.java
@@ -0,0 +1,7 @@
+package com.example.demo.yes.foo.bar.baz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FooBarComponent {
+}
---
+++ 0706demo/src/main/java/com/example/demo/yes/very/deep/pack/DeepComponent.java
@@ -0,0 +1,7 @@
+package com.example.demo.yes.very.deep.pack;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeepComponent {
+}
